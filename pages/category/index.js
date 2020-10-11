0a25b19b625f5699b318bd6ad3704be8af88d4b1
LYF_Yangtz <LYF_Yangtz300>
2020-10-11 16:07:00 +0800

    更新

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,6 @@
+import { request } from "../../request/index"
+import regeneratorRuntime from "../../lib/runtime/runtime"
+
 // pages/category/index.js
 Page({
 
@@ -5,62 +8,71 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    //左侧的菜单数据
+    leftMenuList: [],
+    // 右侧的商品数据
+    rightContent: [],
+    //被点击的左侧的菜单
+    currentIndex: 0
   },
+  //接口的返回数据
+  Cates: [],
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    /* 缓存数据 */
+    const Cates = wx.getStorageSync("cates");
+    if (!Cates) {
+      this.getCates()
+    } else {
+      //设定过期时间
+      if (Date.now() - Cates.time > 1000 * 60 * 10) {
+        this.getCates()
+      } else {
+        this.Cates = Cates.data
+        let leftMenuList = this.Cates.map(v => v.cat_name)
+        let rightContent = this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      }
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //获取分类数据
+  async getCates() {
+    
+    this.Cates = await request({ url: '/categories' })
 
+    wx.setStorageSync("cates", { time: Date.now(), data: this.Cates });
+
+    //构造左侧的大菜单数据
+    let leftMenuList = this.Cates.map(v => v.cat_name)
+    //构造右侧的商品数据
+    let rightContent = this.Cates[0].children;
+    this.setData({
+      leftMenuList,
+      rightContent
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //左侧菜单的点击时间
+  handleItemTap(e) {
+    const { index } = e.currentTarget.dataset
+    /*   request({ url: '/categories' }).then(res => {
+        this.Cates = res.data.message
+        //构造右侧的商品数据
+        let rightContent = this.Cates[index].children;
+        this.setData({
+          rightContent
+        })
+      }) */
+    this.setData({
+      currentIndex: index,
+      rightContent: wx.getStorageSync("cates").data[index].children
+    })
   }
+
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,65 +1,115 @@
+import { request } from "../../request/index"
+import regeneratorRuntime from "../../lib/runtime/runtime"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    //轮播图数组
+    swiperList: [],
+    //导航数组
+    cataList: [],
+    //楼层数据
+    floorList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    //获取轮播图数据
+    //原生请求
+    /* wx.request({
+      url: '/home/swiperdata',
+      success: (result) => {
+        console.log(result);
+        this.setData({
+          swiperList: result.data.message
+        })
+      },
+      fail: () => { },
+      complete: () => { }
+    }); */
+    //封装的请求
+    this.getSwiperList()
+    this.getCataList()
+    this.getFloorList()
   },
+
+  /* 获取轮播图数据 */
+  async getSwiperList() {
+    let res = await request({ url: '/home/swiperdata' })
+    this.setData({
+      swiperList: res
+    })
+  },
+
+  /* 获取导航栏数据 */
+  async getCataList() {
+    let res = await request({ url: '/home/catitems' })
+    this.setData({
+      cataList: res
+    })
+  },
+
+  /* 获取楼层数据 */
+  async getFloorList() {
+    let res = await request({ url: '/home/floordata' })
+    this.setData({
+      floorList: res
+    })
+  },
+
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
